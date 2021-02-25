0cc5ac5daafdd0b6e69cb00c97a14abaef26f4c4
AgentGit <AgentGit@users.noreply.github.com>
2021-02-25 13:48:14 +0100

    Initial Commit

---
+++ SpaceXRockets/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpaceXRockets
+//
+//  Created by Chad Goodyear on 25/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpaceXRockets/SpaceXRocketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXRocketsApp.swift
+//  SpaceXRockets
+//
+//  Created by Chad Goodyear on 25/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXRocketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
